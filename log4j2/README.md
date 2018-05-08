984c877913b4c24b4fcc85179a59ae3f14642c53
zhangwei <928241517@qq.com>
2018-05-09 07:27:21 +0800

    integrate log4j2 to springbot

---
+++ log4j2/README.md
@@ -0,0 +1,3 @@
+#log4j2
+springboot集成log4j2   
+由于springboot1.4开始就不支持log4j了，所以整合log4j2
---
+++ log4j2/src/main/java/com/tech/stack/controller/Log4j2Controller.java
@@ -0,0 +1,13 @@
+package com.tech.stack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhangwei on 2018/5/9.
+ */
+@Controller
+@RequestMapping("log4j2")
+public class Log4j2Controller {
+
+}
