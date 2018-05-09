16cfea3e7ca11d92aada0696b02db6a1765549e8
zhangwei <928241517@qq.com>
2018-05-09 10:08:22 +0800

    usage log4j2

--- log4j2/src/main/java/com/tech/stack/controller/Log4j2Controller.java
+++ log4j2/src/main/java/com/tech/stack/controller/Log4j2Controller.java
@@ -1,5 +1,7 @@
 package com.tech.stack.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("log4j2")
 public class Log4j2Controller {
+
+    static final Logger logger = LogManager.getLogger(Log4j2Controller.class);
+
+    public static void main(String[] args) {
+        //由于配置console的日志级别是info级别，这里的debug日志不会在控制台打印
+        logger.debug("这是debug日志");
+        logger.info("这是info日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志");
+    }
 
 }
