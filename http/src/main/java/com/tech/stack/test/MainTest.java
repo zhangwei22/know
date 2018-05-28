0737078c7ce1a825adaf1fc3a1ea299cf4e2958a
zhangwei <928241517@qq.com>
2018-05-28 13:46:51 +0800

    add a exception test

--- http/src/main/java/com/tech/stack/test/MainTest.java
+++ http/src/main/java/com/tech/stack/test/MainTest.java
@@ -1,29 +1,32 @@
 package com.tech.stack.test;
 
-import com.alibaba.fastjson.JSONObject;
-import com.tech.stack.util.HttpClientUtils;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by zhangwei on 2018/5/14.
  */
 public class MainTest {
     public static void main(String[] args) {
-        Map<String, Object> map = new HashMap();
-        map.put("cityId", 483);
+
+        /**
+         * 如果捕获到异常，则下面的代码会执行
+         * 如果异常没有被捕获，下面的代码不会执行
+         * */
+        String msg = null;
+        System.out.println("这里是会执行的");
         try {
-            String result = HttpClientUtils.sendPost("", map, "");
-            System.out.println("resultStr:" + result);
-            JSONObject obj = JSONObject.parseObject(result);
-            JSONObject json = JSONObject.parseObject(obj.toString());
-            Integer statusCode = json.getInteger("code");
-            System.out.println(statusCode);
-            System.out.println(obj);
-        } catch (IOException e) {
+            msg = new String(null, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("UnsupportedEncodingException");
+            e.printStackTrace();
+        } catch (UnsupportedOperationException e) {
+            System.out.println("UnsupportedOperationException");
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException");
             e.printStackTrace();
         }
+
+        System.out.println("这里还执行吗？");
     }
 }
