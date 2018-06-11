a627346c8559575041834a63fa2531fe2a895911
zhangwei <928241517@qq.com>
2018-06-11 19:19:07 +0800

    add a moudle of time

---
+++ time/src/main/java/com/tech/stack/test/HackingDate.java
@@ -0,0 +1,13 @@
+package com.tech.stack.test;
+
+import java.time.LocalDate;
+
+/**
+ * Created by zhangwei on 2018/6/11.
+ */
+public class HackingDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+    }
+}
